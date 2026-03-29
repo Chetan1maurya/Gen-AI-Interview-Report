5eb3e18fd3a8d54bd80684d010984f52063005ca
Chetan Maurya <mauryachetan27@gmail.com>
2026-03-29 13:39:00 +0530

    Changed port setting

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,6 @@ import connectToDB from './src/config/db.js'
 
 connectToDB()
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running")
 })
